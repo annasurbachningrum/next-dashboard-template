486a8957b9920a1719a9d47bf03c9c1941d99b09
annasurbachningrum <105841109022@student.unismuh.ac.id>
2024-08-15 15:03:24 +0700

    chapter1 selesai

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+import "./ui/global.css";
+import { lusitana } from "./ui/fonts";
+
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body className={`${lusitana.className} antialiased`}>{children}</body>
+      </html>
+    );
+  }
