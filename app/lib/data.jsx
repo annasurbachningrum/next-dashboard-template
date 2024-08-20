8e59c41f5ee58df0ddc92662e56af8592df57881
annasurbachningrum <105841109022@student.unismuh.ac.id>
2024-08-20 17:49:28 +0700

    chapter 8 selesai

--- app/lib/data.jsx
+++ app/lib/data.jsx
@@ -9,9 +9,9 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql`SELECT * FROM revenue`;
+    const data = await sql `SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    console.log("Data fetch completed after 3 seconds.");
 
     return data.rows;
   } catch (error) {
