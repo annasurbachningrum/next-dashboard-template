f43534dbe3924468015dc3f70f9d85db83365ae2
annasurbachningrum <105841109022@student.unismuh.ac.id>
2024-08-20 16:55:46 +0700

    fix code

--- app/ui/dashboard/latest-invoices.jsx
+++ app/ui/dashboard/latest-invoices.jsx
@@ -2,7 +2,7 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '../fonts';
 
 export default async function LatestInvoices({ latestInvoices }) {
   return (
